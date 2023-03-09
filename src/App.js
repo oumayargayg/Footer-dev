c5836ef2925cddcc857db5fcb3b1da89d92a240b
Oumaya <rgaygouma7@gmail.com>
2023-03-09 15:57:40 +0100

    footer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import {Header, Footer , Container } from './Components/Cindex'
+
+
+
+
+
+const App = () => {
+return(
+    <>
+        <Header />
+        <Container />
+        <Footer />
+
+    </>
+)
 }
-
-export default App;
+export default App
---
+++ src/Components/Cindex.js
@@ -0,0 +1,12 @@
+export {default as Header} from './Header/Header'
+export {default as Footer} from './Footer/Footer'
+export {default as Container} from './Container/Container'
+
+
+
+
+
+
+
+
+
